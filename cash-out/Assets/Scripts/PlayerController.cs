abd53c92a2ff81236232108e59d047efeee03df8
Deniz Hınçal <dhinc@uic.edu>
2025-03-14 02:30:31 -0500

    Camera Movement for Player to Look at Cursor

--- cash-out/Assets/Scripts/PlayerController.cs
+++ cash-out/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private float verticalInput;
     private float speed = 10.0f;
 
+    private Vector2 mousePosition; // Variable to store mouse position
+    public float mouseX; // Variable to store mouse X movement
+
     void Start() { }
 
     // Update is called once per frame
@@ -18,5 +21,16 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+
+        var mousePosition = Input.mousePosition; // Get the current mouse position
+        var wantedPosition = Camera.main.ScreenToWorldPoint(
+            new Vector3(mousePosition.x, mousePosition.y, Camera.main.transform.position.y)
+        ); // Convert screen position to world position
+        transform.LookAt(wantedPosition); // Make the player look at the mouse position
+
+        var rotation = transform.rotation;
+        rotation.x = 0; // Reset the x rotation to 0
+        rotation.z = 0; // Reset the z rotation to 0
+        transform.rotation = rotation;
     }
 }
