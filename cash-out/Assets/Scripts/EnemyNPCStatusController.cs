10c33ce758f6af545ebfc791a6370d1496e46820
Deniz Hınçal <dhinc@uic.edu>
2025-03-28 05:02:55 -0500

    Add Basic Health Deduction to Enemy NPC's

---
+++ cash-out/Assets/Scripts/EnemyNPCStatusController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyNPCStatusController : MonoBehaviour
+{
+    public int health = 100; // Health of the enemy NPC
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            // Logic for when the enemy collides with the player
+            health -= 33; // Decrease health by 33 on collision with a bullet
+            if (health <= 0)
+            {
+                Destroy(gameObject); // Destroy the enemy NPC if health is 0 or less
+            }
+        }
+    }
+
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Bullet"))
+    //     {
+    //         // Logic for when the enemy collides with the player
+    //         health -= 33; // Decrease health by 33 on collision with a bullet
+    //         if (health <= 0)
+    //         {
+    //             Destroy(gameObject); // Destroy the enemy NPC if health is 0 or less
+    //         }
+    //     }
+    // }
+}
